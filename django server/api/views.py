b13220488d900896496de2296b715b09fd6f14be
Berrajaa Mohammed <medberrajaa30@gmail.com>
2024-12-18 17:32:29 +0100

    get_output

--- django server/api/views.py
+++ django server/api/views.py
@@ -1,11 +1,13 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 import json
+from model.model import get_output
+
+
 @api_view(['POST'])
 def post(request):
-
     data = json.loads(request.body.decode("utf-8"))
     message = data["message"]
-    
-    return Response({"output" : message})
+    output = get_output(message)
+    return Response({"output" : output})
 
---
+++ django server/model/model.py
@@ -0,0 +1,3 @@
+def get_output(message): 
+
+    return message
